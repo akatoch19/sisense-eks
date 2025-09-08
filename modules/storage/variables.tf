5cf7609e545a6be58fb92ee5069075d1a9306945
unknown <akatoch19@gmail.com>
2025-09-08 21:51:54 +0530

    Updated files

---
+++ modules/storage/variables.tf
@@ -0,0 +1,15 @@
+variable "fsx_storage_capacity" {
+  description = "The storage capacity (in GiB) for FSx Lustre"
+  type        = number
+  default     = 1200
+}
+
+variable "private_subnets" {
+  description = "List of private subnet IDs where FSx will be deployed"
+  type        = list(string)
+}
+
+variable "fsx_sg_id" {
+  description = "Security Group ID for FSx"
+  type        = string
+}
