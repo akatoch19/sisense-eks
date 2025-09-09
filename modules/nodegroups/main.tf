5832abcd9cfbd6e25c6cc6fe25610d7cd55a168b
akatoch19 <32867679+akatoch19@users.noreply.github.com>
2025-09-09 21:24:09 +0530

    Update main.tf

--- modules/nodegroups/main.tf
+++ modules/nodegroups/main.tf
@@ -1,39 +1,52 @@
+locals {
+  node_groups = {
+    sisense-app = {
+      desired_size   = var.desired_size
+      min_size       = var.min_size
+      max_size       = var.max_size
+      instance_types = var.instance_types
+      disk_size      = var.disk_size
+      node_role_arn  = var.node_iam_role
+      labels         = { role = "application" }
+    }
+
+    sisense-query = {
+      desired_size   = var.desired_size
+      min_size       = var.min_size
+      max_size       = var.max_size
+      instance_types = var.instance_types
+      disk_size      = var.disk_size
+      node_role_arn  = var.node_iam_role
+      labels         = { role = "query" }
+    }
+
+    sisense-build = {
+      desired_size   = 1
+      min_size       = 1
+      max_size       = 2
+      instance_types = var.instance_types
+      disk_size      = var.disk_size
+      node_role_arn  = var.node_iam_role
+      labels         = { role = "build" }
+    }
+  }
+}
+
 module "nodegroups" {
   source  = "terraform-aws-modules/eks/aws//modules/eks-managed-node-group"
   version = "20.0.0"
 
-  cluster_name = module.eks.cluster_id
-  subnet_ids   = var.subnet_ids
+  for_each = local.node_groups
 
-  node_groups = {
-    sisense-app = {
-      desired_capacity = var.desired_size
-      min_capacity     = var.min_size
-      max_capacity     = var.max_size
-      instance_types   = var.instance_types
-      disk_size        = var.disk_size
-      node_role_arn    = var.node_iam_role
-      labels           = { role = "application" }
-    }
+  cluster_name   = var.cluster_name
+  subnet_ids     = var.subnet_ids
 
-    sisense-query = {
-      desired_capacity = var.desired_size
-      min_capacity     = var.min_size
-      max_capacity     = var.max_size
-      instance_types   = var.instance_types
-      disk_size        = var.disk_size
-      node_role_arn    = var.node_iam_role
-      labels           = { role = "query" }
-    }
-
-    sisense-build = {
-      desired_capacity = 1
-      min_capacity     = 1
-      max_capacity     = 2
-      instance_types   = var.instance_types
-      disk_size        = var.disk_size
-      node_role_arn    = var.node_iam_role
-      labels           = { role = "build" }
-    }
-  }
+  name           = each.key
+  desired_size   = each.value.desired_size
+  min_size       = each.value.min_size
+  max_size       = each.value.max_size
+  instance_types = each.value.instance_types
+  disk_size      = each.value.disk_size
+  node_role_arn  = each.value.node_role_arn
+  labels         = each.value.labels
 }
