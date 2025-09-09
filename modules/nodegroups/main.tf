2500a314b5cb92e2ab45db04a3b5b70247dbee16
akatoch19 <32867679+akatoch19@users.noreply.github.com>
2025-09-09 20:57:00 +0530

    Update main.tf

--- modules/nodegroups/main.tf
+++ modules/nodegroups/main.tf
@@ -1,42 +1,39 @@
-module "eks" {
- source  = "terraform-aws-modules/eks/aws"
- version = "20.0.0"
+module "nodegroups" {
+  source  = "terraform-aws-modules/eks/aws//modules/eks-managed-node-group"
+  version = "20.0.0"
 
   cluster_name = module.eks.cluster_id
   subnet_ids   = var.subnet_ids
 
-  eks_managed_node_groups = {
+  node_groups = {
     sisense-app = {
-      desired_capacity    = var.desired_size
-      min_capacity        = var.min_size
-      max_capacity        = var.max_size
-      instance_types      = var.instance_types
-      disk_size           = var.disk_size
-      node_role_arn       = var.node_iam_role
-      additional_userdata = templatefile(var.extra_userdata, { ROLE="Application", NAMESPACE=var.namespace })
-      labels = { role = "application" }
+      desired_capacity = var.desired_size
+      min_capacity     = var.min_size
+      max_capacity     = var.max_size
+      instance_types   = var.instance_types
+      disk_size        = var.disk_size
+      node_role_arn    = var.node_iam_role
+      labels           = { role = "application" }
     }
 
     sisense-query = {
-      desired_capacity    = var.desired_size
-      min_capacity        = var.min_size
-      max_capacity        = var.max_size
-      instance_types      = var.instance_types
-      disk_size           = var.disk_size
-      node_role_arn       = var.node_iam_role
-      additional_userdata = templatefile(var.extra_userdata, { ROLE="Query", NAMESPACE=var.namespace })
-      labels = { role = "query" }
+      desired_capacity = var.desired_size
+      min_capacity     = var.min_size
+      max_capacity     = var.max_size
+      instance_types   = var.instance_types
+      disk_size        = var.disk_size
+      node_role_arn    = var.node_iam_role
+      labels           = { role = "query" }
     }
 
     sisense-build = {
-      desired_capacity    = 1
-      min_capacity        = 1
-      max_capacity        = 2
-      instance_types      = var.instance_types
-      disk_size           = var.disk_size
-      node_role_arn       = var.node_iam_role
-      additional_userdata = templatefile(var.extra_userdata, { ROLE="Build", NAMESPACE=var.namespace })
-      labels = { role = "build" }
+      desired_capacity = 1
+      min_capacity     = 1
+      max_capacity     = 2
+      instance_types   = var.instance_types
+      disk_size        = var.disk_size
+      node_role_arn    = var.node_iam_role
+      labels           = { role = "build" }
     }
   }
 }
