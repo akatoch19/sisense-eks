ec697157cccdcd3b920d39e566f806c4a966bf09
akatoch19 <32867679+akatoch19@users.noreply.github.com>
2025-09-10 21:21:25 +0530

    Update variables.tf

--- modules/bastion/variables.tf
+++ modules/bastion/variables.tf
@@ -1,35 +1,6 @@
-variable "env" {
-  type        = string
-  description = "Environment (dev, staging, prod)"
-}
-
-variable "vpc_id" {
-  type        = string
-  description = "VPC ID where bastion will be deployed"
-}
-
-variable "subnet_id" {
-  type        = string
-  description = "Subnet ID for the bastion instance"
-}
-
-variable "ami_id" {
-  type        = string
-  description = "AMI ID for the bastion EC2"
-}
-
-variable "instance_type" {
-  type        = string
-  description = "EC2 instance type"
-  default     = "t3.medium"
-}
-
-variable "key_name" {
-  type        = string
-  description = "SSH key name for EC2 access"
-}
-
-variable "my_ip" {
-  type        = string
-  description = "Your public IP for SSH access"
-}
+variable "env" { type = string }
+variable "vpc_id" { type = string }
+variable "subnet_id" { type = string }
+variable "ami_id" { type = string }
+variable "instance_type" { type = string, default = "t3.medium" }
+variable "key_name" { type = string, default = "" }  # optional with SSM
