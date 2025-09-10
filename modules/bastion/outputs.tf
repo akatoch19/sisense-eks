b8914b3883e4c093028e086ddb76f9e853bf7cc9
akatoch19 <32867679+akatoch19@users.noreply.github.com>
2025-09-10 21:21:45 +0530

    Update outputs.tf

--- modules/bastion/outputs.tf
+++ modules/bastion/outputs.tf
@@ -1,9 +1,9 @@
-output "bastion_public_ip" {
-  value       = aws_instance.bastion.public_ip
-  description = "Public IP of the bastion host"
-}
-
 output "bastion_id" {
   value       = aws_instance.bastion.id
-  description = "EC2 ID of the bastion host"
+  description = "EC2 ID of bastion host"
+}
+
+output "bastion_private_ip" {
+  value       = aws_instance.bastion.private_ip
+  description = "Private IP of the bastion host"
 }
