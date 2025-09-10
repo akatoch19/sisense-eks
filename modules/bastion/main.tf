58e2b4efe3b360d6f59b3aa1bafe9a6c0af2b66b
akatoch19 <32867679+akatoch19@users.noreply.github.com>
2025-09-10 21:17:48 +0530

    Create main.tf

---
+++ modules/bastion/main.tf
@@ -0,0 +1,77 @@
+resource "aws_security_group" "bastion_sg" {
+  name        = "${var.env}-bastion-sg"
+  description = "Security group for EKS bastion host"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.my_ip] # Your IP
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.env}-bastion-sg"
+  }
+}
+
+resource "aws_iam_role" "bastion_role" {
+  name = "${var.env}-eks-bastion-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "bastion_eks_access" {
+  role       = aws_iam_role.bastion_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_iam_instance_profile" "bastion_instance_profile" {
+  name = "${var.env}-bastion-profile"
+  role = aws_iam_role.bastion_role.name
+}
+
+resource "aws_instance" "bastion" {
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  subnet_id                   = var.subnet_id
+  key_name                    = var.key_name
+  associate_public_ip_address = true
+  security_groups             = [aws_security_group.bastion_sg.id]
+  iam_instance_profile        = aws_iam_instance_profile.bastion_instance_profile.name
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              # Install kubectl
+              curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+              chmod +x kubectl
+              mv kubectl /usr/local/bin/
+              # Install AWS CLI
+              curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+              unzip awscliv2.zip
+              ./aws/install
+              # Install Helm
+              curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+              EOF
+
+  tags = {
+    Name = "${var.env}-eks-bastion"
+  }
+}
