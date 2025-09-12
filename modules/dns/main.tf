34a00f5e27c2100dddf71cdc687757a187432210
akatoch19 <32867679+akatoch19@users.noreply.github.com>
2025-09-12 20:55:47 +0530

    Update main.tf

--- modules/dns/main.tf
+++ modules/dns/main.tf
@@ -1,6 +1,7 @@
 #resource "aws_route53_zone" "this" {
   #name = var.zone_name
  # comment = "Sisense DNS Zone for ${var.env}"
+ # tags = var.tags
 #}
 
 #output "zone_id" { value = aws_route53_zone.this.zone_id }
