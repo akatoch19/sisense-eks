f8fa483a2af3b8db9fe51da0632f3b5e05d4c3e7
akatoch19 <32867679+akatoch19@users.noreply.github.com>
2025-09-23 19:04:06 +0530

    Update main.tf

--- modules/dns/main.tf
+++ modules/dns/main.tf
@@ -5,3 +5,26 @@
 #}
 
 #output "zone_id" { value = aws_route53_zone.this.zone_id }
+
+resource "aws_route53_zone" "private_zone" {
+name = var.zone_name # replace with your hosted zone name
+vpc {
+vpc_id = module.vpc.vpc_id
+}
+comment = "Private zone for Sisense staging"
+force_destroy = false
+}
+
+
+resource "aws_route53_record" "app_api" {
+zone_id = aws_route53_zone.private_zone.zone_id
+name = "api.centralsquare-stage.com"
+type = "A"
+
+
+alias {
+name = aws_lb.my_lb.dns_name # replace with your LB resource
+zone_id = aws_lb.my_lb.zone_id
+evaluate_target_health = true
+}
+}
